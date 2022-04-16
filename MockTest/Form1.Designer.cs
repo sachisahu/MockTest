22ac4e1d34d0f71184482550493ff7d167f5fc35
sachisahu <sachisahu0501@gmail.com>
2022-04-16 11:59:29 +0530

    Add project files.

---
+++ MockTest/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace MockTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbCharity = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateMinDate = new System.Windows.Forms.DateTimePicker();
+            this.dateMaxDate = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbCharity
+            // 
+            this.cbCharity.FormattingEnabled = true;
+            this.cbCharity.Location = new System.Drawing.Point(39, 85);
+            this.cbCharity.Name = "cbCharity";
+            this.cbCharity.Size = new System.Drawing.Size(175, 24);
+            this.cbCharity.TabIndex = 0;
+            this.cbCharity.SelectedIndexChanged += new System.EventHandler(this.cbCharity_SelectedIndexChanged);
+            this.cbCharity.SelectedValueChanged += new System.EventHandler(this.cbCharity_SelectedValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Charity";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(272, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Runner Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(275, 86);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(210, 22);
+            this.txtEmail.TabIndex = 3;
+            this.txtEmail.TextChanged += new System.EventHandler(this.txtEmail_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(522, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Registration Min Date";
+            // 
+            // dateMinDate
+            // 
+            this.dateMinDate.Location = new System.Drawing.Point(525, 87);
+            this.dateMinDate.Name = "dateMinDate";
+            this.dateMinDate.Size = new System.Drawing.Size(200, 22);
+            this.dateMinDate.TabIndex = 5;
+            // 
+            // dateMaxDate
+            // 
+            this.dateMaxDate.Location = new System.Drawing.Point(769, 87);
+            this.dateMaxDate.Name = "dateMaxDate";
+            this.dateMaxDate.Size = new System.Drawing.Size(200, 22);
+            this.dateMaxDate.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(766, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Registration Max Date";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(403, 138);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(155, 40);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dgvData
+            // 
+            this.dgvData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(39, 184);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.RowHeadersWidth = 51;
+            this.dgvData.RowTemplate.Height = 24;
+            this.dgvData.Size = new System.Drawing.Size(930, 402);
+            this.dgvData.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(220, 635);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(326, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Total Sponsorship Target of selected Registration:";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Enabled = false;
+            this.txtTotal.Location = new System.Drawing.Point(562, 635);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(163, 22);
+            this.txtTotal.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1028, 689);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.dateMaxDate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateMinDate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbCharity);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbCharity;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateMinDate;
+        private System.Windows.Forms.DateTimePicker dateMaxDate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTotal;
+    }
+}
+
---
+++ MockTest/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MockTest
+{
+    public partial class Form1 : Form
+    {
+        MockTestDBEntities db = new MockTestDBEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadDataCbCharity();
+            loadData();
+            int charityIDGet = (int)cbCharity.SelectedValue;
+            Console.WriteLine(charityIDGet + "");
+        }
+        //Load Data to Data grid
+        private void loadData()
+        {
+
+            int charityIDGet = (int)cbCharity.SelectedValue;
+
+            Console.WriteLine(charityIDGet + "");
+
+            DateTime regnDateMin = dateMinDate.Value.Date;
+            DateTime regnDateMax = dateMaxDate.Value.Date;
+            Console.WriteLine(regnDateMin + " ; " + regnDateMax);
+            var fdata = db.Registrations.Where(x => (x.Charity.CharityId == charityIDGet || charityIDGet == 0) &&
+                                                    (x.Runner.Email.Contains(txtEmail.Text) || txtEmail.Text == "") /*&&
+                                                    (x.RegistrationDateTime.Date >= regnDateMin || dateMinDate == null) &&
+                                                    (x.RegistrationDateTime <= regnDateMax || dateMaxDate == null)*/)
+                .Select(c => new
+                {
+                    RunnersEmail = c.Runner.Email,
+                    RegnDate = c.RegistrationDateTime,
+                    CharityName = c.Charity.CharityName,
+                    SponTragate = c.SponsorshipTarget
+                }).ToList();
+
+            dgvData.DataSource = fdata;
+
+            decimal sum = dgvData.Rows.Cast<DataGridViewRow>().Sum(f => Convert.ToDecimal(f.Cells[3].Value));
+            txtTotal.Text = sum+"";
+
+
+
+            dgvData.Columns["RunnersEmail"].HeaderText = "Runners Email";
+            dgvData.Columns["RegnDate"].HeaderText = "Registration Date Time";
+            dgvData.Columns["CharityName"].HeaderText = "Charity Name";
+            dgvData.Columns["SponTragate"].HeaderText = "Sponsorship target";
+
+        }
+        //load Data to charity Cb Box
+        private void loadDataCbCharity()
+        {
+            var cbCharityList = db.Charities.ToList();
+            cbCharityList.Insert(0,new Charity()
+            {
+                CharityId = 0,
+                CharityName = "All",
+                CharityDescription = "",
+                CharityLogo = ""
+            });
+            
+
+            cbCharity.DataSource = cbCharityList;
+            cbCharity.DisplayMember = "CharityName";
+            cbCharity.ValueMember = "CharityId";
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            loadData();
+        }
+
+        private void cbCharity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //loadData();
+        }
+
+        private void txtEmail_TextChanged(object sender, EventArgs e)
+        {
+            loadData();
+        }
+
+        private void cbCharity_SelectedValueChanged(object sender, EventArgs e)
+        {
+            //loadData();
+        }
+    }
+}
